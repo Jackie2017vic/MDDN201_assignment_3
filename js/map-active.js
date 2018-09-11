8d44fa963477626dc9e75e715288bcae8fc7ec3c
Jackie2017vic <37090831+Jackie2017vic@users.noreply.github.com>
2018-09-12 09:43:37 +1200

    Add files via upload

--- js/map-active.js
+++ js/map-active.js
@@ -1,5 +1,5 @@
 var map;
-var latlng = new google.maps.LatLng(56.9496, 24.1052);
+var latlng = new google.maps.LatLng(75.421780, -41.619600);
 var stylez = [{
     featureType: "all",
     elementType: "all",
@@ -8,7 +8,7 @@ var stylez = [{
             }]
         }];
 var mapOptions = {
-    zoom: 15,
+    zoom: 4,
     center: latlng,
     scrollwheel: false,
     scaleControl: false,
@@ -19,7 +19,7 @@ var mapOptions = {
 };
 map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 var geocoder_map = new google.maps.Geocoder();
-var address = 'Riga';
+var address = 'Greenland';
 geocoder_map.geocode({
     'address': address
 }, function (results, status) {
